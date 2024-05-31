1871ee4ad39842a1f5ae23c0c6713c753ca9ed45
risqinra <risqinraa@gmail.com>
2024-05-30 17:05:44 -0700

    Install Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/cantikk', function () {
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
